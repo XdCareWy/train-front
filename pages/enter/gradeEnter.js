fb0a776855e1c60706d854a04839a63b795f9d1e
zhangxudong21 <zhangxudong6@jd.com>
2018-08-06 21:09:45 +0800

    新增页面

--- pages/enter/gradeEnter.js
+++ pages/enter/gradeEnter.js
@@ -1,67 +1,160 @@
 // pages/enter/gradeEnter.js
-const { BIM_LEVEL, CAD_LEVEL, BIM_APPLY } = getApp().globalData.enterType;
+const {
+  BIM_LEVEL,
+  CAD_LEVEL,
+  BIM_APPLY
+} = getApp().globalData.enterType;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    exampleArray: ['BIM', 'CAD'],
+    items: [{
+        id: 1,
+        name: '3',
+        value: '3人班',
+        checked: 'true'
+      },
+      {
+        id: 2,
+        name: '5',
+        value: '5人班'
+      },
+      {
+        id: 3,
+        name: '10',
+        value: '10人班'
+      }
+    ],
+    examineType: [{
+        id: 0,
+        name: 'BIM一级'
+      },
+      {
+        id: 1,
+        name: '二级建筑'
+      },
+      {
+        id: 2,
+        name: '二级结构'
+      },
+      {
+        id: 3,
+        name: '二级设备'
+      }
+    ],
     exampleIndex: 0,
     idArray: ['大学生', '其他'],
     idIndex: 0,
-    msg: ""
+    msg: "",
+    user: {
+      id: 1,
+      name: "张旭东"
+    },
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // id: 1-BIM等级考试；2-CAD等级考试；3-BIM技能应用
     let cardId = options.id;
-    let exampleArray = ['BIM', 'CAD'];
-    let exampleIndex = 0;
+    let examineType = [];
     switch (+cardId) {
       case BIM_LEVEL:
+        examineType = [{
+            id: 0,
+            name: 'BIM一级'
+          },
+          {
+            id: 1,
+            name: '二级建筑'
+          },
+          {
+            id: 2,
+            name: '二级结构'
+          },
+          {
+            id: 3,
+            name: '二级设备'
+          }
+        ];
+        break;
       case CAD_LEVEL:
-        exampleIndex = cardId - 1;
+        examineType = [{
+          id: 0,
+          name: 'CAD一级'
+        }];
         break;
       case BIM_APPLY:
-        exampleArray = [
-          { value: 'Revit', name: 'revit'},
-          { value: '案例实操', name: 'operate' }
+        examineType = [{
+            id: 0,
+            name: 'Revit基础与提高'
+          },
+          {
+            id: 1,
+            name: '案例实操'
+          },
+          {
+            id: 2,
+            name: 'Revit基础与提高+案例实操'
+          }
         ];
-        exampleIndex = -1;
         break;
       default:
-        exampleIndex = 0;
+        examineType = [{
+            id: 0,
+            name: 'BIM一级'
+          },
+          {
+            id: 1,
+            name: '二级建筑'
+          },
+          {
+            id: 2,
+            name: '二级结构'
+          },
+          {
+            id: 3,
+            name: '二级设备'
+          }
+        ];;
     }
-    this.setData({ exampleIndex: exampleIndex, exampleArray: exampleArray })
+    this.setData({
+      examineType: examineType,
+    })
   },
-  bindPickerChange: function (e) {
+  bindPickerChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       exampleIndex: e.detail.value
     })
   },
-  bindIdPickerChange: function (e) {
+  bindIdPickerChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       idIndex: e.detail.value
     })
   },
-  showErrorMsg: function (filed, msg) {
+  showErrorMsg: function(filed, msg) {
     if (filed === "" || filed.length === 0) {
-      this.setData({ msgVisible: true, msg: msg });
+      this.setData({
+        msgVisible: true,
+        msg: msg
+      });
       const t = setTimeout(() => {
-        this.setData({ msgVisible: false, msg: "" });
+        this.setData({
+          msgVisible: false,
+          msg: ""
+        });
         t && clearTimeout(t);
       }, 2000);
       return false;
     }
     return true;
   },
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     const requireFiled = ["name", "phone", "idCard", "idType", "address", "exampleType"];
     const requireValue = {
       name: "姓名不能为空",
@@ -72,7 +165,14 @@ Page({
       exampleType: "选择考试类别"
     };
     const data = e.detail.value;
-    const { name, phone, idCard, idType, address, exampleType } = data;
+    const {
+      name,
+      phone,
+      idCard,
+      idType,
+      address,
+      exampleType
+    } = data;
     for (let k in data) {
       let r = requireFiled.includes(k) && this.showErrorMsg(data[k], requireValue[k]);
       if (!r) {
@@ -98,5 +198,10 @@ Page({
     //     // todo: 处理错误
     //   }
     // })
+  },
+  handleStudentTap: function() {
+    wx.navigateTo({
+      url: '../student/list/list',
+    })
   }
 })
---
+++ pages/student/add/add.js
@@ -0,0 +1,22 @@
+// pages/student/add/add.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    idArray: ['大学生', '其他'],
+    idIndex: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  formSubmit: function() {
+    
+  }
+})
